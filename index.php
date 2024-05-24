95c6e78183c8cb12ba74e1760b19b0e5ae4bc5d2
Maxwell Hogg <maxy.hogg@googlemail.com>
2024-05-24 10:03:15 +0100

    updated css/main.css

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/main.css">
+    <title>Scrippt</title>
+</head>
+<body>
+    <nav>
+        <a class="logo" href="index.php">
+            SCRIPPT
+        </a>
+        <a class="entry" href="">Add Entry</a>
+    </nav>
+</body>
+</html>
