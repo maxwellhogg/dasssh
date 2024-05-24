71b44b866e423c244716d0fd11b660a983fe4c62
Maxwell Hogg <maxy.hogg@googlemail.com>
2024-05-24 11:32:42 +0100

    updated index.php and main.css

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Nokora:wght@100;300;400;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./css/main.css">
     <title>Scrippt</title>
 </head>
@@ -11,7 +17,23 @@
         <a class="logo" href="index.php">
             SCRIPPT
         </a>
-        <a class="entry" href="">Add Entry</a>
+        <a class="entry" href="">
+           + Add Entry
+        </a>
     </nav>
+    <main>
+        <h1 class="main-heading">SCRIPPTS</h1>
+        <div>
+            <div class="card">
+                <h2 class="card-heading">Dolor Magna Eget Est Lorem Ipsum</h2>
+            </div>
+            <div class="card">
+                <h2 class="card-heading">Dolor Magna Eget Est Lorem Ipsum</h2>
+            </div>
+            <div class="card">
+                <h2 class="card-heading">Dolor Magna Eget Est Lorem Ipsum</h2>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
