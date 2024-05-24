13a5b098af1e87e739b9258dc30264884b222b6b
Maxwell Hogg <maxy.hogg@googlemail.com>
2024-05-24 14:56:59 +0100

    updated style.css

--- index.php
+++ index.php
@@ -9,30 +9,87 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Nokora:wght@100;300;400;700;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Scrippt</title>
 </head>
 <body>
     <nav>
-        <a class="logo" href="index.php">
-            SCRIPPT
-        </a>
-        <a class="entry" href="">
-           + Add Entry
-        </a>
+        <div class="container">
+            <div class="nav-container">
+                <a class="logo" href="index.php">
+                    SCRIPPT
+                </a>
+                <a class="entry" href="">
+                + Add Entry
+                </a>
+            </div>
+        </div>
     </nav>
     <main>
         <h1 class="main-heading">SCRIPPTS</h1>
-        <div>
+        <div class="container">
             <div class="card">
-                <h2 class="card-heading">Dolor Magna Eget Est Lorem Ipsum</h2>
+                <div class="image-container">
+                    <img class="thumbnail" src="images/jason.png" alt="">
+                </div>
+                <div class="content-container">
+                    <div class="content-date">May 22 2024</div>
+                    <h2 class="card-heading">Dolor Magna Eget Est Lorem Ipsum</h2>
+                    <p class="content-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Facilisis leo vel fringilla est. Sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra. Pellentesque sit amet porttitor eget dolor morbi non arcu. Id consectetur purus ut faucibus pulvinar elementum integer. Nibh nisl condimentum id venenatis a condimentum vitae. Purus sit amet luctus venenatis lectus magna fringilla urna. Ac tortor dignissim convallis aenean et. Scelerisque in dictum non consectetur a erat. Quam vulputate dignissim suspendisse in. Malesuada pellentesque elit eget gravida cum sociis. Facilisi cras fermentum odio eu feugiat pretium.</p>
+                </div>
             </div>
             <div class="card">
-                <h2 class="card-heading">Dolor Magna Eget Est Lorem Ipsum</h2>
+                <div class="image-container">
+                    <img class="thumbnail" src="images/hallloween.png" alt="">
+                </div>
+                <div class="content-container">
+                    <div class="content-date">May 20 2024</div>
+                    <h2 class="card-heading">Dolor Magna Eget Est Lorem Ipsum</h2>
+                    <p class="content-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque viverra mauris in aliquam sem fringilla. Pulvinar elementum integer enim neque volutpat ac. Tellus id interdum velit laoreet id donec ultrices tincidunt. Parturient montes nascetur ridiculus mus mauris vitae ultricies leo integer. Ut sem nulla pharetra diam sit. Nisl vel pretium lectus quam. Sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis. Lobortis elementum nibh tellus molestie. Morbi leo urna molestie at elementum. Id nibh tortor id aliquet lectus proin nibh nisl condimentum. Blandit cursus risus at ultrices mi. Ipsum a arcu cursus vitae congue.</p>
+                </div>
             </div>
             <div class="card">
-                <h2 class="card-heading">Dolor Magna Eget Est Lorem Ipsum</h2>
+                <div class="image-container">
+                    <img class="thumbnail" src="images/slasher.png" alt="">
+                </div>
+                <div class="content-container">
+                    <div class="content-date">May 18 2024</div>
+                    <h2 class="card-heading">Dolor Magna Eget Est Lorem Ipsum</h2>
+                    <p class="content-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean et tortor at risus viverra adipiscing at in tellus. Placerat vestibulum lectus mauris ultrices eros in cursus. Diam sit amet nisl suscipit adipiscing bibendum est ultricies integer. Lectus sit amet est placerat in. Varius vel pharetra vel turpis nunc eget lorem dolor. Nisl nunc mi ipsum faucibus vitae aliquet. Sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis. Quis vel eros donec ac odio tempor orci.</p>
+                </div>
             </div>
+            <div class="card">
+                <div class="image-container">
+                    <img class="thumbnail" src="images/jason2.png" alt="">
+                </div>
+                <div class="content-container">
+                    <div class="content-date">May 17 2024</div>
+                    <h2 class="card-heading">Dolor Magna Eget Est Lorem Ipsum</h2>
+                    <p class="content-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aenean et tortor at risus viverra adipiscing at in tellus. Placerat vestibulum lectus mauris ultrices eros in cursus. Diam sit amet nisl suscipit adipiscing bibendum est ultricies integer. Lectus sit amet est placerat in. Varius vel pharetra vel turpis nunc eget lorem dolor. Nisl nunc mi ipsum faucibus vitae aliquet. Sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis. Quis vel eros donec ac odio tempor orci.</p>
+                </div>
+            </div>
+            <ul class="pagination">
+                <li class="pagination-item">
+                    <a class="pagination-link" href="#">&laquo;</a>
+                </li>
+                <li class="pagination-item pagination-item-active">
+                    <a class="pagination-link" href="#">1</a></li>
+                <li class="pagination-item">
+                    <a class="pagination-link" href="#">2</a>
+                </li>
+                <li class="pagination-item">
+                    <a class="pagination-link" href="#">...</a>
+                </li>
+                <li class="pagination-item">
+                    <a class="pagination-link" href="#">9</a>
+                </li>
+                <li class="pagination-item">
+                    <a class="pagination-link" href="#">10</a>
+                </li>
+                <li class="pagination-item">
+                    <a class="pagination-link" href="#">&raquo;</a>
+                </li>
+            </ul>
         </div>
     </main>
 </body>
