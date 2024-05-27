5df808824c913790d83ea3939e74fbffbb993fc7
Maxwell Hogg <maxy.hogg@googlemail.com>
2024-05-27 20:38:14 +0100

    added file upload functionality to form.php

--- form.php
+++ form.php
@@ -9,10 +9,38 @@ if (!empty($_POST))
     $date       =   (string) ($_POST['date'] ?? '');
     $article    =   (string) ($_POST['entry'] ?? '');
 
-    $stmt = $pdo->prepare('INSERT INTO `entries` (`title`, `date`, `article`) VALUES (:title, :date, :article)');
+    if (!empty($_FILES) && !empty($_FILES['image'])) {
+        if ($_FILES['image']['error'] === 0 && $_FILES['image']['size'] !== 0) {
+            $removedExtension = pathinfo($_FILES['image']['name'], PATHINFO_FILENAME);
+            $name = preg_replace('/[^a-zA-Z0-9]/', '', $removedExtension);
+
+            $originalImage = $_FILES['image']['tmp_name'];
+            $imageDestinationExt = $name .  '-' . time() . '.jpg';
+            $imageDestination = __DIR__ . '/uploads/' . $imageDestinationExt;
+
+            [$width, $height] = getimagesize($originalImage);
+
+            $maxDimensions = 400;
+            $scaleFactor = $maxDimensions / max($width, $height);
+
+            $newWidth = $width * $scaleFactor;
+            $newHeight = $height * $scaleFactor;
+
+            $im = imagecreatefromjpeg($originalImage);
+
+            $newImage = imagecreatetruecolor($newWidth, $newHeight);
+            imagecopyresampled($newImage, $im, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+
+            imagejpeg($newImage, $imageDestination);
+            
+        }
+    }
+
+    $stmt = $pdo->prepare('INSERT INTO `entries` (`title`, `date`, `article`, `image`) VALUES (:title, :date, :article, :image)');
     $stmt->bindValue(':title', $title);
     $stmt->bindValue(':date', $date);
     $stmt->bindValue(':article', $article);
+    $stmt->bindValue(':image', $imageDestinationExt);
     $stmt->execute();
 
     echo '<a href="index.php">Continue to the display page</a>';
@@ -23,7 +51,7 @@ if (!empty($_POST))
 
 <?php require __DIR__ . '/views/header.views.php'; ?>
 <h1 class="main-heading">NEW ENTRY</h1>
-<form class="form" method="POST" action="form.php">
+<form class="form" method="POST" action="form.php" enctype="multipart/form-data">
     <div class="form-section">
         <label class="form-section-item" for="title">Title:</label>
         <input class="form-section-item-input" type="text" id="title" name="title" required />
@@ -31,6 +59,10 @@ if (!empty($_POST))
     <div class="form-section">
         <label class="form-section-item" for="date">Date:</label>
         <input class="form-section-item-input" type="date" id="date" name="date" required />
+    </div>
+    <div class="form-section">
+        <label class="form-section-item" for="image">Image (Not Required):</label>
+        <input class="form-section-item-input" type="file" id="image" name="image" required />
     </div>
     <div class="form-section">
         <label class="form-section-item" for="entry">Entry:</label>
--- index.php
+++ index.php
@@ -3,9 +3,20 @@
 require __DIR__ . '/inc/functions.inc.php';
 require __DIR__ . '/inc/db-connect.inc.php';
 
-$perPage = 2;
+date_default_timezone_set('Etc/GMT+0');
+
+$perPage = 6;
 $page = $_GET['page'] ?? 1;
+if ($page < 1) $page = 1;
+
 $offset = ($page - 1) * $perPage;
+
+$stmtCount = $pdo->prepare('SELECT COUNT(*) AS `count` FROM `entries`');
+$stmtCount->execute();
+$count = $stmtCount->fetch(PDO::FETCH_ASSOC)['count'];
+
+$numPages = ceil($count / $perPage);
+
 $stmt = $pdo->prepare('SELECT * FROM `entries` ORDER BY `date` DESC, `id` DESC LIMIT :perPage OFFSET :offset');
 $stmt->bindValue('perPage', (int) $perPage, PDO::PARAM_INT);
 $stmt->bindValue('offset', (int) $offset, PDO::PARAM_INT);
@@ -14,36 +25,46 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 <?php require __DIR__ . '/views/header.views.php'; ?>
-<h1 class="main-heading">ARTICLES</h1>
+<h1 class="main-heading">Messages</h1>
 <?php foreach ($results as $result): ?>
 <div class="card">
-    <div class="image-container">
-        <img class="thumbnail" src="images/jason.png" alt="">
-    </div>
+    <?php if (!empty($result['image'])): ?>
+        <div class="image-container">
+            <img class="thumbnail" src="images/jason.png" alt="">
+        </div>
+    <?php endif; ?>
     <div class="content-container">
-        <div class="content-date"><?= esc($result['date']); ?></div>
+        <?php 
+            $dateExploded = explode('-', $result['date']);
+            $timestamp = mktime(12, 0, 0, $dateExploded[1], $dateExploded[2], $dateExploded[0]);
+        ?>
+        <div class="content-date"><?= esc(date('jS-M-Y', $timestamp)); ?></div>
         <h2 class="card-heading"><?= esc($result['title']); ?></h2>
         <p class="content-description"><?= nl2br(esc($result['article'])); ?></p>
     </div>
 </div>
 <?php endforeach; ?>
-<ul class="pagination">
-    <li class="pagination-item">
-        <a class="pagination-link pagination-link-arrow" href="#">&laquo;</a>
-    </li>
-    <li class="pagination-item pagination-item-active">
-        <a class="pagination-link pagination-link-active" href="#">1</a></li>
-    <li class="pagination-item">
-        <a class="pagination-link" href="#">2</a>
-    </li>
-    <li class="pagination-item">
-        <a class="pagination-link" href="#">3</a>
-    </li>
-    <li class="pagination-item">
-        <a class="pagination-link" href="#">4</a>
-    </li>
-    <li class="pagination-item">
-        <a class="pagination-link pagination-link-arrow" href="#">&raquo;</a>
-    </li>
-</ul>
+<?php if ($numPages > 1): ?>
+    <ul class="pagination">
+        <?php if ($page > 1): ?>
+            <li class="pagination-item">
+                <a class="pagination-link pagination-link-arrow" href="index.php?<?= http_build_query(['page' => $page - 1]); ?>">&laquo;</a>
+            </li>
+        <?php endif ?>
+        <?php for($x = 1; $x <= $numPages; $x++): ?>
+            <li class="pagination-item">
+                <a 
+                    class="pagination-link <?php if ($page === $x): ?>pagination-link-active<?php endif; ?>"
+                    href="index.php?<?= http_build_query(['page' => $x]); ?>">
+                    <?= esc($x); ?>
+                </a>
+            </li>
+        <?php endfor; ?>
+        <?php if ($page < $numPages): ?>
+            <li class="pagination-item">
+                <a class="pagination-link pagination-link-arrow" href="index.php?<?= http_build_query(['page' => $page + 1]) ?>">&raquo;</a>
+            </li>
+        <?php endif; ?>
+    </ul>
+<?php endif; ?>
 <?php require __DIR__ . '/views/footer.views.php'; ?>
